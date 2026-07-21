b6649a110319bce7aa37f5b7897dd50710948bb5
Chu Thanh Tinh <cttinh0812@gmail.com>
2026-07-21 15:50:33 +0700

    delete test file

--- test_login.js
+++
@@ -1,4 +0,0 @@
-const { loginUser } = require('./src/lib/services/auth-service');
-loginUser({ email: 'student@fpt.edu.vn', password: 'password123' })
-  .then(console.log)
-  .catch(console.error);
